128fbd5dcf5687913fa2370fc07a70b2b3e64821
lego <mr.re66it@gmail.com>
2016-02-25 14:00:49 +0200

    v 0.11 Lego & Nick

--- app/src/main/java/com/legonick/cardgames/Activity/MainMenuActivity.java
+++ app/src/main/java/com/legonick/cardgames/Activity/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package com.legonick.cardgames.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.legonick.cardgames.R;
 
@@ -11,7 +13,10 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
 
-
+    public void PlayWithBot(View view) {
+        Intent playScreen = new Intent(MainMenuActivity.this, PlayActivity.class);
+        startActivity(playScreen);
     }
 }
--- app/src/main/java/com/legonick/cardgames/Activity/PlayActivity.java
+++ app/src/main/java/com/legonick/cardgames/Activity/PlayActivity.java
@@ -1,7 +1,17 @@
 package com.legonick.cardgames.Activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.legonick.cardgames.R;
+
 /**
  * Created by Lego on 24.02.2016.
  */
-public class PlayActivity {
+public class PlayActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_activity);
+    }
 }
