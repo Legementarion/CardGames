6417b4c9498706d1b728d74072bb1090d6dfbe46
Legementario <mr.re66it@gmail.com>
2016-02-25 22:11:04 +0200

    v 0.32

--- app/src/main/java/com/legonick/cardgames/Activity/PlayActivity.java
+++ app/src/main/java/com/legonick/cardgames/Activity/PlayActivity.java
@@ -2,7 +2,7 @@ package com.legonick.cardgames.Activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.widget.LinearLayout;
 
 import com.legonick.cardgames.R;
 
@@ -10,10 +10,13 @@ import com.legonick.cardgames.R;
  * Created by Lego on 24.02.2016.
  */
 public class PlayActivity extends Activity{
+    LinearLayout background = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_activity);
+        background = (LinearLayout) findViewById(R.id.background);
+//        background.setTileModeX(android.graphics.Shader.TileMode.REPEAT );
     }
 }
