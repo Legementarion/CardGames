c82a7850f0814347df25ac415cf9452eaa6cb7c4
Legementario <mr.re66it@gmail.com>
2016-02-27 19:40:07 +0200

    v 0.82

--- app/src/main/java/com/legonick/cardgames/Activity/PlayActivity.java
+++ app/src/main/java/com/legonick/cardgames/Activity/PlayActivity.java
@@ -16,9 +16,9 @@ import com.legonick.cardgames.R;
 /**
  * Created by Lego on 24.02.2016.
  */
-public class PlayActivity extends Activity{
-//    GameCore game;
-    Game game;
+public class PlayActivity extends Activity {
+    GameCore game;
+//    Game game;
 
     ImageButton exit_button = null;
     ImageButton stop_button = null;
@@ -32,34 +32,37 @@ public class PlayActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_activity);
-        exit_button = (ImageButton)findViewById(R.id.Exit);
-        stop_button = (ImageButton)findViewById(R.id.Stop);
-        new_button = (ImageButton)findViewById(R.id.NewGame);
-        pick_button = (ImageButton)findViewById(R.id.Pick);
+        exit_button = (ImageButton) findViewById(R.id.Exit);
+        stop_button = (ImageButton) findViewById(R.id.Stop);
+        new_button = (ImageButton) findViewById(R.id.NewGame);
+        pick_button = (ImageButton) findViewById(R.id.Pick);
 
         Resources res = getResources();
-        for (int i=0; i<imageViewsOfUser.length; i++){
-            imageViewsOfBot[i] = (ImageView)findViewById(res.getIdentifier("b"+i, "id", this.getPackageName()));
-            imageViewsOfUser[i] = (ImageView)findViewById(res.getIdentifier("p"+i, "id", this.getPackageName()));
+        for (int i = 0; i < imageViewsOfUser.length; i++) {
+            imageViewsOfBot[i] = (ImageView) findViewById(res.getIdentifier("b" + i, "id", this.getPackageName()));
+            imageViewsOfUser[i] = (ImageView) findViewById(res.getIdentifier("p" + i, "id", this.getPackageName()));
             System.out.println(imageViewsOfBot[i].toString());
             System.out.println(imageViewsOfUser[i].toString());
         }
 
-//        game = GameCore.getInstance(this);
-        game = Game.getInstance(this);
+        game = GameCore.getInstance(this);
+//        game = Game.getInstance(this);
     }
 
     public void buttonStop(View view) {
-        game.endGame();
+        game.stopGame();
     }
+
     public void buttonExit(View view) {
         Intent playScreen = new Intent(PlayActivity.this, MainMenuActivity.class);
         startActivity(playScreen);
     }
+
     public void buttonNew(View view) {
         ((ImageView) findViewById(R.id.Status)).setImageResource(R.drawable.nullcard);
         game.startGame();
     }
+
     public void buttonPick(View view) {
         game.userPick();
     }
